885df0b330b37237fc33fec2b0ac67dc25af849f
Pramith Chathuranga <pramith@intervest.lk>
2021-12-27 00:17:30 +0530

    new changes added.

--- components/image.js
+++ components/image.js
@@ -1,15 +1,17 @@
 import { getStrapiMedia } from "../lib/media";
 
 const Image = ({ image, style }) => {
-  const stringToAdd = 'f_auto/';
-  const imageUrlRaw = getStrapiMedia(image);
+  // const stringToAdd = 'f_auto/';
+  // const imageUrlRaw = getStrapiMedia(image);
+
+  const imageUrl = getStrapiMedia(image);
   
-  function addStr(imageUrl, index, stringToAdd){
-    return imageUrl.substring(0, index) + stringToAdd + imageUrl.substring(index, imageUrl.length);
-  }
+  // function addStr(imageUrl, index, stringToAdd){
+  //   return imageUrl.substring(0, index) + stringToAdd + imageUrl.substring(index, imageUrl.length);
+  // }
 
   // console.log(addStr(imageUrlRaw, 50, stringToAdd));
-  const imageUrl = addStr(imageUrlRaw, 50, stringToAdd);
+  // const imageUrl = addStr(imageUrlRaw, 50, stringToAdd);
   // console.log(imageUrl); https://res.cloudinary.com/pramithex/image/upload/v1632671244/Home_2d2a93aee4.png
   return (
     // eslint-disable-next-line @next/next/no-img-element
