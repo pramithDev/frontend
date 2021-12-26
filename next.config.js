3acf23638388b978146f27bc13b4d64ad92ff6d0
Pramith Chathuranga <pramith@intervest.lk>
2021-12-26 23:37:01 +0530

    new styling changes added.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     images: {
         loader: 'cloudinary',
         domains: ["res.cloudinary.com"],
-        path: "https://res.cloudinary.com/pramithex/image/upload/",
+        path: "https://res.cloudinary.com/pramithex/image/upload/f_auto/",
         disableStaticImages: true,
     },
     trailingSlash: true,
