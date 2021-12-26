7e9885309693626591fc189ac5100be3ecb56d56
Pramith Chathuranga <pramith@intervest.lk>
2021-12-26 23:25:24 +0530

    new changes added..

--- components/image.js
+++ components/image.js
@@ -1,8 +1,16 @@
 import { getStrapiMedia } from "../lib/media";
 
 const Image = ({ image, style }) => {
-  const imageUrl = getStrapiMedia(image);
+  const stringToAdd = 'f_auto/';
+  const imageUrlRaw = getStrapiMedia(image);
+  
+  function addStr(imageUrl, index, stringToAdd){
+    return imageUrl.substring(0, index) + stringToAdd + imageUrl.substring(index, imageUrl.length);
+  }
 
+  // console.log(addStr(imageUrlRaw, 50, stringToAdd));
+  const imageUrl = addStr(imageUrlRaw, 50, stringToAdd);
+  // console.log(imageUrl); https://res.cloudinary.com/pramithex/image/upload/v1632671244/Home_2d2a93aee4.png
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     images: {
         loader: 'cloudinary',
         domains: ["res.cloudinary.com"],
+        path: "https://res.cloudinary.com/pramithex/image/upload/",
         disableStaticImages: true,
     },
     trailingSlash: true,
